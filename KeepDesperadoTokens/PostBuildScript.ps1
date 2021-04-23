81c3bc76db7319deb5a3f4d32e0642d2e6b6359e
Florian <floseipp@gmx.de>
2021-04-23 23:05:35 +0200

    fix: added the manifest into the repo
    also added post build script for better automation

---
+++ KeepDesperadoTokens/PostBuildScript.ps1
@@ -0,0 +1,7 @@
+$compress = @{
+  Path = "..\icon.png", "..\manifest.json", "..\README.md", ".\bin\Debug\netstandard2.0\KeepDesperadoTokens.dll"
+  CompressionLevel = "Fastest"
+  DestinationPath = "..\KeepDesperadoTokens.zip"
+}
+Compress-Archive @compress
+exit 0
