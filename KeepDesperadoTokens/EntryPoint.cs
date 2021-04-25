7e8e0db33481387872ca71bd2fcb376a1549ddeb
Florian <floseipp@gmx.de>
2021-04-25 22:50:49 +0200

    refactor: refactor code
    Created an actual entry point for all hooks and such

---
+++ KeepDesperadoTokens/EntryPoint.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Logging;
+using On.RoR2;
+using On.EntityStates.GameOver;
+
+namespace KeepDesperadoTokens
+{
+    [BepInPlugin("com.nutty.keepDesperadoTokens", "keepDesperadoTokens", "1.0.1")]
+    public class EntryPoint : BaseUnityPlugin
+    {
+        internal new static ManualLogSource Logger { get; private set; }
+
+        private readonly KeepDesperadoTokens _keepDesperadoTokens = new KeepDesperadoTokens();
+        private readonly LightsoutRewards _lightsoutRewards = new LightsoutRewards();
+
+        public EntryPoint()
+        {
+            Logger = base.Logger;
+
+            CharacterBody.RecalculateStats += _keepDesperadoTokens.RecalculateTokenAmount;
+            ShowReport.OnEnter += _keepDesperadoTokens.OnRunEndResetTokens;
+        }
+
+        public void Awake()
+        {
+            InitConfig();
+            _keepDesperadoTokens.Init();
+            _lightsoutRewards.Init();
+        }
+
+        private void InitConfig()
+        {
+            _keepDesperadoTokens.TokenMultiplier = Config.Bind(
+                "Settings",
+                "TokenMultiplier",
+                0.25,
+                $"The multiplier for your tokens on stage change. Has to be written in decimal, i.e.: 1.0 = 100%, 0.5 = 50%, etc.{Environment.NewLine}Minimum: {_keepDesperadoTokens.Min}, Maximum: {_keepDesperadoTokens.Max}"
+            );
+        }
+
+    }
+}
--- KeepDesperadoTokens/KeepDesperadoTokens.cs
+++ KeepDesperadoTokens/KeepDesperadoTokens.cs
@@ -1,63 +1,50 @@
 ﻿using System;
-using BepInEx;
 using BepInEx.Configuration;
 using RoR2;
 
-namespace ROR2_Infinite
+namespace KeepDesperadoTokens
 {
-	[BepInPlugin("com.nutty.keepDesperadoTokens", "keepDesperadoTokens", "1.0.0")]
-	public class KeepDesperadoTokens : BaseUnityPlugin
-	{
-		public static int MaxDesperadoTokens;
+    public class KeepDesperadoTokens
+    {
+        private int _maxDesperadoTokens;
 
-		public static ConfigEntry<double> TokenMultiplier { get; set; }
-		public KeepDesperadoTokens()
-		{
-			On.RoR2.CharacterBody.RecalculateStats += this.CharacterBody_RecalculateStats;
-			On.EntityStates.GameOver.ShowReport.OnEnter += this.ShowReport_OnEnter;
-		}
-		public void Awake()
-        {
-			double min = 0.0;
-			double max = 1.0;
-			InitConfig();
-			TokenMultiplier.Value = Math.Min(Math.Max(TokenMultiplier.Value, min), max);
-		}
+        public ConfigEntry<double> TokenMultiplier { get; set; }
+        public double Min { get; } = 0;
+        public double Max { get; } = 1;
 
-		private void ShowReport_OnEnter(On.EntityStates.GameOver.ShowReport.orig_OnEnter orig, EntityStates.GameOver.ShowReport self)
-		{
-			KeepDesperadoTokens.MaxDesperadoTokens = 0;
-			orig(self);
-		}
-		private void CharacterBody_RecalculateStats(On.RoR2.CharacterBody.orig_RecalculateStats orig, RoR2.CharacterBody self)
-		{
-			if (self.isPlayerControlled && self.teamComponent.teamIndex == TeamIndex.Player)
-			{
-				int currentTokenAmount = self.GetBuffCount(RoR2.RoR2Content.Buffs.BanditSkull);
-				int newTokenAmount = (int)Math.Floor(KeepDesperadoTokens.MaxDesperadoTokens * TokenMultiplier.Value);
-				if (currentTokenAmount < newTokenAmount)
-				{
-					for (int i = 0; i < newTokenAmount - currentTokenAmount; i++)
-					{
-						self.AddBuff(RoR2.RoR2Content.Buffs.BanditSkull);
-					}
-				}
-				if (currentTokenAmount > KeepDesperadoTokens.MaxDesperadoTokens)
-				{
-					KeepDesperadoTokens.MaxDesperadoTokens = self.GetBuffCount(RoR2.RoR2Content.Buffs.BanditSkull);
-				}
-			}
-			orig(self);
-		}
-		private void InitConfig()
+        public void Init()
         {
-			TokenMultiplier = Config.Bind<double>(
-				"Settings",
-				"TokenMultiplier",
-				0.25,
-				"The multiplier for your tokens on stage change. Has to be written in decimal, i.e.: 1.0 = 100%, 0.5 = 50%, etc.\n"
-				+ "Minimum: 0.0, Maximum: 1.0"
-			);
-		}
-	}
+            TokenMultiplier.Value = Math.Min(Math.Max(TokenMultiplier.Value, Min), Max);
+        }
+
+        public void OnRunEndResetTokens(On.EntityStates.GameOver.ShowReport.orig_OnEnter orig, EntityStates.GameOver.ShowReport self)
+        {
+            _maxDesperadoTokens = 0;
+            orig(self);
+        }
+
+        public void RecalculateTokenAmount(On.RoR2.CharacterBody.orig_RecalculateStats orig, CharacterBody self)
+        {
+            if (self.isPlayerControlled && self.teamComponent.teamIndex == TeamIndex.Player)
+            {
+                int currentTokenAmount = self.GetBuffCount(RoR2Content.Buffs.BanditSkull);
+                int newTokenAmount = (int)Math.Floor(_maxDesperadoTokens * TokenMultiplier.Value);
+
+                if (currentTokenAmount < newTokenAmount)
+                {
+                    for (int i = 0; i < newTokenAmount - currentTokenAmount; i++)
+                    {
+                        self.AddBuff(RoR2Content.Buffs.BanditSkull);
+                    }
+                }
+
+                if (currentTokenAmount > _maxDesperadoTokens)
+                {
+                    _maxDesperadoTokens = self.GetBuffCount(RoR2Content.Buffs.BanditSkull);
+                }
+            }
+
+            orig(self);
+        }
+    }
 }
